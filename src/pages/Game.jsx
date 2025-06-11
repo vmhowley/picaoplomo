b10cd1c743df75618eca88686ac521cd939bcc7d
vmhowley <vmhowleyh@gmail.com>
2025-06-11 10:01:05 -0400

    first

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { cards } from "../data/cards";
+import { cards } from "../data/Cards.js";
 
 const Game = () => {
   const location = useLocation();
